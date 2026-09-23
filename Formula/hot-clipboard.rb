ef7800639327c5cffd2b043b2a6bec917e250874
Whatfck <danieleperez12@gmail.com>
2026-09-23 17:26:36 -0500

    Update SHA256 checksum in formula

--- Formula/hot-clipboard.rb
+++ Formula/hot-clipboard.rb
@@ -2,7 +2,7 @@ class HotClipboard < Formula
   desc "Ergonomic macOS clipboard CLI bridging terminal and NSPasteboard"
   homepage "https://github.com/Whatfck/hot-clipboard"
   url "https://github.com/Whatfck/hot-clipboard/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "419cb83add375793e4f30cb360e73148eb0e962aeb5e675d94f4a7478cdf604c"
+  sha256 "394f17104530efe0fbec391664ae3341fcc2fe0f2d990807001f8f57a0b70970"
   license "MIT"
   head "https://github.com/Whatfck/hot-clipboard.git", branch: "develop"
 
